d0025a6e8bd18711d48f6d71f488318ce12a6022
surajaiswal13 <surajaiswal13@gmail.com>
2021-09-01 03:18:20 +0530

    Updated 4.1

--- Easy Access Multi Db App/app.py
+++ Easy Access Multi Db App/app.py
@@ -532,15 +532,18 @@ def mongo_download_data():
             os.chmod(os.path.join(os.getcwd()+'\\static\\files'), 0o777)
             shutil.rmtree(os.path.join(os.getcwd()+'\\static\\files'), ignore_errors=True)
             os.mkdir(os.path.join(os.getcwd()+'\\static',"files"))
+            print(os.path.join(os.getcwd()+'\\static',"files"))
             # time.sleep(8)
             MongoClient = MongoDBManagement(url=session['url'],password=session['mongo_password'])
             file = MongoClient.downloadDataFromCollection(db_name=database_name, collection_name=collection_name, file_name=new_filename)
             print(file)            
-
-
+            # print("dcvs")
+            new_path = os.path.join(os.getcwd()+"\\"+file)
+            print(new_path)
+            return send_file(new_path, as_attachment=True, mimetype="text/csv")
+            
             # flash('Data Downloaded Successfully as '+new_filename+".csv")
             # return redirect('/mongo/download_data')
-            return send_file(file, as_attachment=True, mimetype="text/csv")
 
             # return render_template('mongodb/download_data.html')
 
